c0298c3d4b3bea668c66f2f6d4c470ff9af87aab
abhikatta <abhinaykatta97@gmail.com>
2025-05-16 14:05:36 +0530

    added GET api for logged in user data

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView
-from constants import SIGNUP_URL
+from .views import SignUpView, CurrentUserView
+from constants import SIGNUP_URL, USER_INFO_URL
 
 urlpatterns = [
     path(SIGNUP_URL, SignUpView.as_view(), name="signup"),
+    path(USER_INFO_URL, CurrentUserView.as_view(), name="user_info"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+
 from .serializers import SignUpSerializer
 
 
@@ -20,3 +24,20 @@ class SignUpView(APIView):
                 serializer.to_representation(user), status=status.HTTP_201_CREATED
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+User = get_user_model()
+
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        return Response(
+            {
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+            }
+        )
